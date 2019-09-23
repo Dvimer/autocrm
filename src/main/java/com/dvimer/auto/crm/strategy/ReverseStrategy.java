1d9518969caa77b732e7e33d2f964387a31d6d83
Nosov_AM <am.nosov@bpcbt.com>
2019-09-23 17:02:53 +0300

    delite strategy reverse

--- src/main/java/com/dvimer/auto/crm/strategy/ReverseStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.dvimer.auto.crm.strategy;
-
-import org.hibernate.tool.internal.reveng.DefaultReverseEngineeringStrategy;
-
-public class ReverseStrategy extends DefaultReverseEngineeringStrategy {
-
-}
